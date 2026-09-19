a20a3ef6c1fabf0eac9d464332c9393610c8a79e
pyetrosoares150-max <pyetrosoares150@gmail.com>
2026-09-18 22:28:57 -0300

    add

---
+++ script.js
@@ -0,0 +1,6 @@
+let anosdexp = document.getElementById('textojs')
+let arrey = ['H', 'á', ' ', 'M', 'a', 'i', 's', ' ','d', 'e', ' ', '3', '5', ' ', 'a', 'n', 'o', 's']
+let armzem=''
+arrey.forEach(function(elemento){
+    console.log(arrey)
+});
